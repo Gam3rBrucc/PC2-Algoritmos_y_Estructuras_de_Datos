6eeb8db31b79ad6127fb01a5e93e5e5ed5ba9c1a
Bruce <brucematiasfleck@gmail.com>
2022-04-30 00:18:50 -0500

    Create CTaxista.h

---
+++ CTaxista.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <iostream>
+#include "CPersona.h"
+#include "CAutomovil.h"
+
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
+
+class CTaxista : protected CPersona {
+protected:
+    CAutomovil automovil;
+public:
+    CTaxista() {}
+    CTaxista(string n, string a, short e, int ndc, string c, CAutomovil au) : CPersona(n,a,e,ndc,c) {
+        automovil = au;
+    }
+
+    string mostrar_info() {
+        return nombre + " " + apellido + ", " + std::to_string(edad) + "\nNumero de celular: " + std::to_string(numero_de_celular) + "\nAutomovil: " + automovil.info() + "\nContrasenia: " + contrasenia + "\n\n";
+    }
+};
