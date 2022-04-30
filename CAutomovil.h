234fbf6f326b08e431ca7b10405c6b2b90228318
Bruce <brucematiasfleck@gmail.com>
2022-04-30 00:14:36 -0500

    Create CAutomovil.h

---
+++ CAutomovil.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <iostream>
+
+using std::string;
+
+class CAutomovil {
+protected:
+    string modelo_automovil;
+    string placa_automovil;
+
+public:
+    CAutomovil(string ma = "-", string pa = "-") {
+        modelo_automovil = ma;
+        placa_automovil = pa;
+    }
+
+    string info() {
+        return modelo_automovil + " [" + placa_automovil + "]";
+    }
+};
