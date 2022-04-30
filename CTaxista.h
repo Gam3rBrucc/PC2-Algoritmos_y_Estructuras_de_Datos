f5c06c5fa43c67b6c6294bbe021325aa32f59591
Bruce <brucematiasfleck@gmail.com>
2022-04-30 07:53:20 -0500

    Update CTaxista.h

--- CTaxista.h
+++ CTaxista.h
@@ -20,4 +20,11 @@ public:
     string mostrar_info() {
         return nombre + " " + apellido + ", " + std::to_string(edad) + "\nNumero de celular: " + std::to_string(numero_de_celular) + "\nAutomovil: " + automovil.info() + "\nContrasenia: " + contrasenia + "\n\n";
     }
+
+    int get_numero_celular() {
+        return numero_de_celular;
+    }
+    string get_contrasenia() {
+        return contrasenia;
+    }
 };
