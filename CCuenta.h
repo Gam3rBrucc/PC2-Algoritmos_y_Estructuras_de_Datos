d79c9971479f58708fbb9094d80bcbd880c4c2c4
Bruce <brucematiasfleck@gmail.com>
2022-04-30 08:12:49 -0500

    Update CCuenta.h

--- CCuenta.h
+++ CCuenta.h
@@ -1,5 +1,9 @@
 #pragma once
 #include <iostream>
+#include "CUsuario.h"
+#include "CTaxista.h"
+#include "CLista.h"
+#include "Archivo.h"
 
 using std::string;
 using std::cout;
@@ -7,5 +11,12 @@ using std::cin;
 using std::endl;
 
 class CCuenta {
-    // TODO desarrollar clase CCuenta
+
+public:
+    CCuenta(CUsuario obj) { // Corre si el obj pasado es de usuario
+        cout << "Cuento usuario works!\n";
+    }
+    CCuenta(CTaxista obj) { // Corre si el obj pasado es de taxista
+        cout << "Cuenta taxista works!\n";
+    }
 };
