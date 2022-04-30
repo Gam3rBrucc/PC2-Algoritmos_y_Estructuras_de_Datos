960ee1ee179120aa5e29b2f7066f23675f9458a4
Bruce <brucematiasfleck@gmail.com>
2022-04-30 00:19:08 -0500

    Create CUsuario.h

---
+++ CUsuario.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include "CPersona.h"
+
+using std::string;
+
+class CUsuario : protected CPersona {
+
+public:
+    CUsuario() {}
+    CUsuario(string n, string a, short e, int ndc, string c) : CPersona(n,a,e,ndc,c) {
+
+    }
+
+    string mostrar_info() {
+        return nombre + " " + apellido + ", " + std::to_string(edad) + "\nNumero de celular: " + std::to_string(numero_de_celular) + "\nContrasenia: " + contrasenia + "\n\n";
+    }
+};
