bd0c3732abedb48a050ad5801488d4ecd68b4d20
Bruce <brucematiasfleck@gmail.com>
2022-04-30 07:53:49 -0500

    Update CUsuario.h

--- CUsuario.h
+++ CUsuario.h
@@ -16,4 +16,11 @@ public:
     string mostrar_info() {
         return nombre + " " + apellido + ", " + std::to_string(edad) + "\nNumero de celular: " + std::to_string(numero_de_celular) + "\nContrasenia: " + contrasenia + "\n\n";
     }
+
+    int get_numero_celular() {
+        return numero_de_celular;
+    }
+    string get_contrasenia() {
+        return contrasenia;
+    }
 };
