6ba350df2fff0fb391508751f036165e6c3f23e9
Bruce <brucematiasfleck@gmail.com>
2022-05-01 01:11:43 -0500

    Update CCuenta.h

--- CCuenta.h
+++ CCuenta.h
@@ -5,20 +5,85 @@
 #include "CLista.h"
 #include "Archivo.h"
 
-// TODO falta desarrollar la classe CCuenta
-
 using std::string;
 using std::cout;
 using std::cin;
 using std::endl;
 
-class CCuenta {
+// TODO falta desarrollar la classe CCuenta
 
+class CCuenta {
+    CUsuario usuario;
+    CTaxista taxista;
 public:
     CCuenta(CUsuario obj) { // Corre si el obj pasado es de usuario
-        cout << "Cuento usuario works!\n";
+        usuario = obj;
+        cout << "====================== Cuenta ======================\n\n";
+        cout << "\t[1] Ver datos de cuenta\n";
+        cout << "\t[2] Ver historial de viajes\n";
+        cout << "\t[3] Cambiar datos\n";
+        cout << "\t[4] Pedir taxi\n";
+        cout << "\t[5] Eliminar cuenta\n";
+        cout << "\t[R] Regresar\n\n";
+        cout << "Elija una opcion: ";
+
+        string input;
+        cin >> input;
+        while(input != "1" && input != "2" && input != "3" && input != "4" && input != "5" && input != "r" && input != "R") {
+            cout << "~~Lo sentimos pero no entendemos lo que ha ingresado, por favor elija una opcion de nuevo\n";
+            cin >> input;
+        }
+
+        if(input == "1") { // Ver datos de cuenta
+            cout << "=============== Datos de cuenta ===============\n\n";
+            cout << usuario.mostrar_info();
+            CCuenta cuenta_U(usuario);
+        } else if(input == "2") { // Ver historial de viajes
+
+        } else if(input == "3") { // Cambiar datos
+
+        } else if(input == "4") { // Pedir taxi
+
+        } else if(input == "5") { // Eliminar cuenta
+
+        } else if(input == "r" || input == "R") { // Regresar pagina
+
+        } else cout << "\n\n\t~ERROR~\n\n";
     }
     CCuenta(CTaxista obj) { // Corre si el obj pasado es de taxista
-        cout << "Cuenta taxista works!\n";
+        taxista = obj;
+        cout << "====================== Cuenta ======================\n\n";
+        cout << "\t[1] Ver datos de cuenta\n";
+        cout << "\t[2] Ver historial de pasajeros\n";
+        cout << "\t[3] Cambiar datos\n";
+        cout << "\t[4] Buscar pasajero\n";
+        cout << "\t[5] Registrar automovil\n";
+        cout << "\t[6] Eliminar cuenta\n";
+        cout << "\t[R] Regresar\n\n";
+        cout << "Elija una opcion: ";
+
+        string input;
+        cin >> input;
+        while(input != "1" && input != "2" && input != "3" && input != "4" && input != "5" && input != "6" && input != "r" && input != "R") {
+            cout << "~~Lo sentimos pero no entendemos lo que ha ingresado, por favor elija una opcion de nuevo\n";
+            cin >> input;
+        }
+
+        if(input == "1") { // Ver datos de cuenta
+            cout << taxista.mostrar_info();
+            CCuenta cuenta_T(taxista);
+        } else if(input == "2") { // Ver historial de viajes
+
+        } else if(input == "3") { // Cambiar datos
+
+        } else if(input == "4") { // Buscar pasajero
+
+        } else if(input == "5") { // Registrar automovil
+
+        } else if(input == "6") { // Eliminar cuenta
+
+        } else if(input == "r" || input == "R") { // Regresar pagina
+
+        } else cout << "\n\n\t~ERROR~\n\n";
     }
 };
