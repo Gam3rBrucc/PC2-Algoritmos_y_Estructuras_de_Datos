a474c7e5b9176a7233a9236a0fd1c2cd1ab06388
Bruce <brucematiasfleck@gmail.com>
2022-05-02 13:09:06 -0500

    Update CUsuario.h

--- CUsuario.h
+++ CUsuario.h
@@ -2,17 +2,28 @@
 #include <iostream>
 #include <string>
 #include "CPersona.h"
+#include "CViaje.h"
+#include "CPila.h"
 
 using std::string;
 
 class CUsuario : protected CPersona {
-
+    size_t t_arreglo;
+    CViaje viajes_arreglo[0];
+    CPila<CViaje>* viajes;
 public:
     CUsuario() {}
-    CUsuario(string n, string a, short e, int ndc, string c) : CPersona(n,a,e,ndc,c) {
+    CUsuario(string n, string a, short e, int ndc, string c) : CPersona(n,a,e,ndc,c) {}
+
+    void preparar_para_almacenamiento() {
 
     }
+    void sacar_de_almacenamiento() {
 
+    }
+    void agregar_viaje(CViaje v) {
+        viajes->push(v);
+    }
     string mostrar_info() {
         return nombre + " " + apellido + ", " + std::to_string(edad) + "\nNumero de celular: " + std::to_string(numero_de_celular) + "\nContrasenia: " + contrasenia + "\n\n";
     }
