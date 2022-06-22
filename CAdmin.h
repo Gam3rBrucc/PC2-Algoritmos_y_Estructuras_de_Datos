6717027f13df8527e37e8e1a06aafbef8374c3dc
Bruce <brucematiasfleck@gmail.com>
2022-06-21 20:14:57 -0500

    Changed use of customs lists to std::vector

--- CAdmin.h
+++ CAdmin.h
@@ -1,174 +1,85 @@
 #pragma once
+#ifndef UNTITLED10_CADMIN_H
+#define UNTITLED10_CADMIN_H
+
+#endif //UNTITLED10_CADMIN_H
 #include <iostream>
 #include <string>
-#include "CLista.h"
+#include <vector>
 #include "Archivo.h"
 #include "CUsuario.h"
 #include "CTaxista.h"
 
-using std::string;
-using std::cout;
-using std::cin;
-using std::endl;
-
-#define ARCH_U "usuarios.bin" // ARCH_U se refiere a ARCHivo Usuarios
-#define ARCH_T "taxistas.bin" // ARCH_T se refiere a ARCHivo Taxistas
-
-// TODO Implementa algoritmo de ordenamiento
+// TODO Implementa algoritmo de ordenamiento de stl
+// TODO hacer que la opcion 1 muestre 10 objetos a la vez y que el usuario pueda elegir que numero de pagina ver
 
 class CAdmin {
     bool encontro_usuario = false;
     bool encontro_taxista = false;
-    CUsuario* arreglo_usuarios;
-    CTaxista* arreglo_taxistas;
 public:
-    CAdmin() {
-        cout << "=================== Modo Admin ===================\n";
-        cout << "\n\t[1] Ver informacion de todos las cuentas registradas\n";
-        cout << "\t[2] Eliminar cuenta\n";
-        cout << "\t[3] Ver fibonacci\n";
-        cout << "\t[R] Regresar\n";
-        cout << "\nElija una opcion: ";
-
-//        string input;
-//        cin >> input;
-//        while(input != "1" && input != "2" && input != "r" && input != "R") {
-//            cout << "~~Lo sentimos pero no entendemos lo que ha ingresado, por favor elija una opcion de nuevo\n";
-//            cin >> input;
-//        }
+    CAdmin(std::vector<CUsuario>* lista_usuarios, std::vector<CTaxista>* lista_taxistas) {
+        printf("=================== Modo Admin ===================\n");
+        printf("\n\t[1] Ver informacion de todos las cuentas registradas\n");
+        printf("\t[2] Eliminar cuenta\n");
+        printf("\t[R] Regresar\n");
+        printf("\nElija una opcion: ");
 
         char input;
-        cin >> input;
+        scanf(" %c",&input);
         input = revisar_input(input);
 
-        CLista<CUsuario> lista_usuarios;
-        CLista<CTaxista> lista_taxistas;
-
-        leerObjetosGuardados<CUsuario>(ARCH_U, &lista_usuarios);
-        leerObjetosGuardados<CTaxista>(ARCH_T, &lista_taxistas);
-
         if (input == '1') { // Recorre las listas de usuarios y taxistas mostrando toda la informacion de estas
-            // TODO implementarlo aqui
-            cout << "====================== Lista de usuarios ======================\n\n";
-            bubble_sort(lista_usuarios);
-            //lista_usuarios.recorrer_inicio([](CUsuario o) { cout << o.mostrar_info(); });
-            cout << "====================== Lista de taxistas ======================\n\n";
-            insertion_sort(lista_taxistas);
-            //lista_taxistas.recorrer_inicio([](CTaxista o) { cout << o.mostrar_info(); });
+            printf("====================== Lista de usuarios ======================\n\n");
+            for(auto i : *lista_usuarios) printf("%s", i.mostrar_info().c_str());
+            printf("====================== Lista de taxistas ======================\n\n");
+            for(auto i : *lista_taxistas) printf("%s", i.mostrar_info().c_str());
 
-            CAdmin();
+            CAdmin(lista_usuarios, lista_taxistas);
 
         } else if (input == '2') { // Eliminar cuenta
-            cout << "Para eliminar esta cuenta necesitas ingresar el numero de celular y contrasenia del usuario.\n\n";
-            cout << "\tNumero de celular: ";
+            printf("Para eliminar esta cuenta necesitas ingresar el numero de celular y contrasenia del usuario.\n\n");
+            printf("\tNumero de celular: ");
 
             int numero_de_celular;
-            cin >> numero_de_celular;
-            cout << "\tContrasenia: ";
-            string contrasenia;
-            cin >> contrasenia;
+            scanf("%d", &numero_de_celular);
+            printf("\tContrasenia: ");
+            std::string contrasenia;
+            std::cin >> contrasenia;
 
-            lista_usuarios.recorrer_inicio(
-                    [=](CUsuario o) { // Recorre toda la lista de usuarios y revisa si el numero de celular y contrasenia coinciden
-                        if (numero_de_celular == o.get_numero_celular() &&
-                            contrasenia == o.get_contrasenia())
-                            encontro_usuario = true;
-                    });
-            lista_taxistas.recorrer_inicio(
-                    [=](CTaxista o) { // Recorre toda la lista de usuarios y revisa si el numero de celular y contrasenia coinciden
-                        if (numero_de_celular == o.get_numero_celular() &&
-                            contrasenia == o.get_contrasenia())
-                            encontro_taxista = true;
-                    });
+            long iU = 0, iT = 0; // indiceUsuario, indiceTaxista
+            for(auto i : *lista_usuarios) {
+                if(numero_de_celular == i.get_num_cel() && contrasenia == i.get_password()) {
+                    encontro_usuario = true;
+                    break;
+                }
+                ++iU;
+            }
+            for(auto i : *lista_taxistas) {
+                if(numero_de_celular == i.get_num_cel() && contrasenia == i.get_password()) {
+                    encontro_taxista = true;
+                    break;
+                }
+                ++iT;
+            }
 
             if (encontro_usuario) { // Si el usuario de encontro, recorre toda la lista guardando cada objecto en el archivo SIN el usuario que el admin a eligido, efectivamente eliminandolo
-                limpiarArchivo(ARCH_U);
-                lista_usuarios.recorrer_inicio([=](CUsuario o) {
-                    if (numero_de_celular != o.get_numero_celular()) {
-                        guardarObjeto(ARCH_U, o);
-                    }
-                });
+                lista_usuarios->erase(lista_usuarios->begin()+iU);
             } else if (encontro_taxista) { // Si el taxista de encontro, recorre toda la lista guardando cada objecto en el archivo SIN el taxista que el admin a eligido, efectivamente eliminandolo
-                limpiarArchivo(ARCH_T);
-                lista_taxistas.recorrer_inicio([=](CTaxista o) {
-                    if (numero_de_celular != o.get_numero_celular()) {
-                        guardarObjeto(ARCH_T, o);
-                    }
-                });
-            } else cout << "\n~~No se encontro ningun cuenta con ese numero de celular o contrasenia\n";
+                lista_taxistas->erase(lista_taxistas->begin()+iT);
+            } else printf("\n~~No se encontro ningun cuenta con ese numero de celular o contrasenia\n");
 
-            if (encontro_usuario || encontro_taxista) cout << "\n~~Se logro eliminar la cuenta!\n";
+            if (encontro_usuario || encontro_taxista) printf("\n~~Se logro eliminar la cuenta!\n");
 
-            CAdmin();
-
-        } else if(input == '3') {
-            cout << "\nPor favor ingrese un numero: ";
-            size_t num;
-            cin >> num;
-            for(size_t i=0; i<num; ++i) {
-                cout << "[" << calcular_fibonacci(i) << "] ";
-            }
-            cout << endl;
-            CAdmin();
+            CAdmin(lista_usuarios, lista_taxistas);
         } else if(input == 'r' || input == 'R') { // Regresar pagina
-        } else cout << "\n\n\t~ERROR~\n\n";
+        } else printf("\n\n\t~ERROR~\n\n");
     }
     char revisar_input(char in) {
         char ingr = in;
         if(in != '1' && in != '2' && in != '3' && in != 'r' && in != 'R') {
-            cout << "~~Lo sentimos pero no entendemos lo que ha ingresado, por favor elija una opcion de nuevo: ";
-            cin >> ingr;
+            printf("~~Lo sentimos pero no entendemos lo que ha ingresado, por favor elija una opcion de nuevo: ");
+            scanf(" %c",&ingr);
             return revisar_input(ingr);
         } else return ingr;
-    }
-    void bubble_sort(CLista<CUsuario> lista_usuario) {
-        arreglo_usuarios = new CUsuario[lista_usuario.get_t()];
-        size_t i = 0;
-        lista_usuario.recorrer_inicio([&](CUsuario o){
-            arreglo_usuarios[i] = o;
-            ++i;
-        });
-        CUsuario aux;
-        size_t cap = lista_usuario.get_t() - 1;
-        for(size_t i=0; i<lista_usuario.get_t(); ++i) {
-            for(size_t j=0; j<cap; ++j) {
-                if(arreglo_usuarios[j].get_nombre().compare(arreglo_usuarios[j+1].get_nombre()) > arreglo_usuarios[j+1].get_nombre().compare(arreglo_usuarios[j].get_nombre())) {
-                    aux = arreglo_usuarios[j];
-                    arreglo_usuarios[j] = arreglo_usuarios[j+1];
-                    arreglo_usuarios[j+1] = aux;
-                }
-            }
-            --cap;
-        }
-        for(size_t i=0; i<lista_usuario.get_t(); ++i) {
-            cout << arreglo_usuarios[i].mostrar_info();
-        }
-    }
-    void insertion_sort(CLista<CTaxista> lista_taxista) {
-        arreglo_taxistas = new CTaxista[lista_taxista.get_t()];
-        size_t i = 0;
-        lista_taxista.recorrer_inicio([&](CTaxista o){
-            arreglo_taxistas[i] = o;
-            ++i;
-        });
-        CTaxista aux_taxista;
-        size_t j;
-        for(size_t i=0; i<lista_taxista.get_t(); ++i) {
-            aux_taxista = arreglo_taxistas[i];
-            j = i - 1;
-            while(j>=0 && arreglo_taxistas[j].get_nombre().compare(aux_taxista.get_nombre()) > aux_taxista.get_nombre().compare(arreglo_usuarios[j].get_nombre())) {
-                arreglo_taxistas[j+1] = arreglo_taxistas[j];
-                --j;
-            }
-            arreglo_taxistas[j+1] = aux_taxista;
-        }
-        for(size_t i=0; i<lista_taxista.get_t(); ++i) {
-            cout << arreglo_taxistas[i].mostrar_info();
-        }
-    }
-    size_t calcular_fibonacci(size_t n) {
-        if(n == 0) return 0;
-        if(n == 1) return 1;
-        return calcular_fibonacci(n-1) + calcular_fibonacci(n - 2);
     }
 };
