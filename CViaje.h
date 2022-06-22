46f0260bf0be50dc22585af240f082bdff3a99b9
Bruce <brucematiasfleck@gmail.com>
2022-06-22 07:35:41 -0500

    ye

--- CViaje.h
+++ CViaje.h
@@ -1,27 +1,26 @@
 #pragma once
+#ifndef UNTITLED10_CVIAJE_H
+#define UNTITLED10_CVIAJE_H
+
+#endif //UNTITLED10_CVIAJE_H
 #include <iostream>
-#include "CTaxista.h"
 #include <string>
-
-using std::string;
-
 class CViaje {
 public:
-    string ubicacion;
-    string destino;
+    std::string ubicacion;
+    std::string destino;
     short calificacion;
-    CTaxista chofer;
-
+    std::string nombre_chofer;
+    std::string automovil_chofer;
     CViaje() {}
-    CViaje(string u, string d, short cal, CTaxista cho) {
-        ubicacion = u;
-        destino = d;
-        calificacion = cal;
-        chofer = cho;
+    CViaje(std::string u, std::string d, short cal, std::string cho, std::string aut) {
+        this->ubicacion = u;
+        this->destino = d;
+        this->calificacion = cal;
+        this->nombre_chofer = cho;
+        this->automovil_chofer = aut;
     }
-
-    string mostrar_info() {
-        return "Ubicacion: " + ubicacion + "\nDestino: " + destino + "Chofer: " + chofer.get_nombre() + " " + chofer.get_apellido() + "\n\tAutomovil: " + chofer.info_auto() + "\nCalificacion: " +
-                std::to_string(calificacion) + "\n";
+    std::string mostrar_info() {
+        return "Ubicacion: " + ubicacion + "\nDestino: " + destino + "\nChofer: " + nombre_chofer + "\n\tAutomovil: " + automovil_chofer + "\nCalificacion: " + std::to_string(calificacion) + "\n\n";
     }
 };
