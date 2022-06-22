723f9de07ceb464193bf33b65a4992d74fb26742
Bruce <brucematiasfleck@gmail.com>
2022-06-21 20:23:58 -0500

    Some changes

--- CLogin.h
+++ CLogin.h
@@ -1,65 +1,51 @@
 #pragma once
+#ifndef UNTITLED10_CLOGIN_H
+#define UNTITLED10_CLOGIN_H
+
+#endif //UNTITLED10_CLOGIN_H
 #include <iostream>
+#include <vector>
 #include "CUsuario.h"
 #include "CTaxista.h"
 #include "Archivo.h"
-#include "CLogin.h"
 #include "CCuenta.h"
-
-using std::string;
-using std::cout;
-using std::cin;
-using std::endl;
-
-#define ARCH_U "usuarios.bin" // ARCH_U se refiere a ARCHivo Usuarios
-#define ARCH_T "taxistas.bin" // ARCH_T se refiere a ARCHivo Taxistas
-
 class CLogin {
     int numero_de_celular;
-    string contrasenia;
+    std::string contrasenia;
 
     CUsuario cuenta_usuario; // Aqui se guardara el objeto de la cuenta de usuario si se encuentra
     CTaxista cuenta_taxista; // Aqui se guardara el objeto de la cuenta de taxista si se encuentra
-    CLista<CUsuario> lista_usuarios;
-    CLista<CTaxista> lista_taxistas;
 
     bool encontro_usuario = false;
     bool encontro_taxista = false;
 public:
-    CLogin() {
+    CLogin(std::vector<CUsuario>* lista_usuarios, std::vector<CTaxista>* lista_taxistas) {
+        printf("================== Login ==================\n\n");
+        printf("\tNumero de celular: ");
+        scanf("%d",&numero_de_celular);
+        printf("\tContrasenia: ");
+        std::cin >> contrasenia;
 
-
-        cout << "================== Login ==================\n\n";
-        cout << "\tNumero de celular: ";
-        cin >> numero_de_celular;
-        cout << "\tContrasenia: ";
-        cin >> contrasenia;
-
-        leerObjetosGuardados<CUsuario>(ARCH_U, &lista_usuarios);
-        leerObjetosGuardados<CTaxista>(ARCH_T, &lista_taxistas);
-
-
-        lista_usuarios.recorrer_inicio([=](CUsuario o){ // Recorre toda la lista de usuarios y revisa si el numero de celular y contrasenia coinciden
-           if(numero_de_celular == o.get_numero_celular() && contrasenia == o.get_contrasenia()) {
-               cuenta_usuario = o;
-               encontro_usuario = true;
-           }
-        });
-        lista_taxistas.recorrer_inicio([=](CTaxista o){ // Recorre toda la lista de usuarios y revisa si el numero de celular y contrasenia coinciden
-            if(numero_de_celular == o.get_numero_celular() && contrasenia == o.get_contrasenia()) {
-                cuenta_taxista = o;
-                encontro_taxista = true;
+        for(auto i : *lista_usuarios) {
+            if (numero_de_celular == i.get_num_cel() && contrasenia == i.get_password()) {
+                cuenta_usuario = i;
+                encontro_usuario = true;
+                break;
             }
-        });
-
+        }
+        for(auto i : *lista_taxistas) {
+            if (numero_de_celular == i.get_num_cel() && contrasenia == i.get_password()) {
+                cuenta_taxista = i;
+                encontro_taxista = true;
+                break;
+            }
+        }
         if(encontro_usuario) {
-            cout << "\nBienvenido " << cuenta_usuario.get_nombre() << "!!!\n";
-            CCuenta cuenta_U(cuenta_usuario); // Crea un objeto de CCuenta para poder pasarle el objecto de usuario
-        }
-        else if(encontro_taxista) {
-            cout << "\nBienvenido " << cuenta_taxista.get_nombre() << "!!!\n";
-            CCuenta cuenta_T(cuenta_taxista); // Crea un objeto de CCuenta para poder pasarle el objecto de taxista
-        }
-        else cout << "\n~~No se encontro ningun cuenta con ese numero de celular o contrasenia\n";
+            printf("\nBienvenido %s!!!\n",cuenta_usuario.get_nombre().c_str());
+            CCuenta cuenta_U(cuenta_usuario, lista_usuarios, lista_taxistas); // Crea un objeto de CCuenta para poder pasarle el objecto de usuario
+        } else if(encontro_taxista) {
+            printf("\nBienvenido %s!!!\n",cuenta_taxista.get_nombre().c_str());
+            CCuenta cuenta_T(cuenta_taxista, lista_usuarios, lista_taxistas); // Crea un objeto de CCuenta para poder pasarle el objecto de taxista
+        } else printf("\n~~No se encontro ningun cuenta con ese numero de celular o contrasenia\n");
     }
 };
