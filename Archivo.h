a28a495679a6dd30bf1fa7f18c36fc3e5d839695
Bruce <brucematiasfleck@gmail.com>
2022-06-21 20:13:57 -0500

    Changed saving method to C equivalent

--- Archivo.h
+++ Archivo.h
@@ -1,45 +1,72 @@
 #pragma once
+#ifndef UNTITLED10_ARCHIVO_H
+#define UNTITLED10_ARCHIVO_H
+
+#endif //UNTITLED10_ARCHIVO_H
 #include <iostream>
-#include <functional>
-#include <fstream>
-#include "CLista.h"
+#include <vector>
+#include "CUsuario.h"
+#include "CTaxista.h"
 
-using std::string;
-using std::cout;
-using std::cin;
-using std::endl;
-using std::ios;
-
-void limpiarArchivo(string archivo) // Para cambiar datos, se guarda la lista y se crea una nueva pero con los datos nuevos
-{
-    std::ofstream limpiar;
-    limpiar.open(archivo, ios::binary);
-    if(limpiar.is_open()) {}
-    else cout << "~~ERROR, No se pudo abrir el archivo \"" << archivo << "\"\n";
+void limpiarArchivo(std::string nombre_arch) {
+    FILE* archivo = fopen(nombre_arch.c_str(), "w");
+    if(archivo == NULL) printf("~~ERROR, No se pudo abrir el archivo \"%s\"\n",nombre_arch.c_str());
 }
-
-template <typename Generico>
-void guardarObjeto(string archivo, Generico obj) {
-    std::ofstream guardar;
-    guardar.open(archivo, ios::binary | ios::app);
-    if(guardar.is_open()) {
-        guardar.write((char*)&obj, sizeof(Generico));
-        guardar.close();
-    } else cout << "~~ERROR, No se pudo abrir el archivo \"" << archivo << "\"\n";
+void guardarObjeto(std::string nombre_arch, CUsuario obj) {
+    FILE* archivo = fopen(nombre_arch.c_str(), "a");
+    if(archivo != NULL) {
+        fprintf(archivo, "%s %s %s %hi %d\n",obj.get_nombre().c_str(), obj.get_apellido().c_str(), obj.get_password().c_str(), obj.get_edad(), obj.get_num_cel());
+        fclose(archivo);
+    } else printf("~~ERROR, No se pudo abrir el archivo \"%s\"\n",nombre_arch.c_str());
 }
-
-template <typename Generico>
-void leerObjetosGuardados(string archivo, CLista<Generico>* lista) {
-    std::ifstream leer;
-    leer.open(archivo, ios::binary);
-    if(leer.is_open()) {
-        while(leer.eof() == false) {
-            Generico n;
-            leer.read((char*)&n, sizeof(Generico));
-            lista->push_back(n);
+void guardarObjeto(std::string nombre_arch, CTaxista obj) {
+    FILE* archivo = fopen(nombre_arch.c_str(), "a");
+    if(archivo != NULL) {
+        fprintf(archivo, "%s %s %s %hi %d %s %s\n",obj.get_nombre().c_str(), obj.get_apellido().c_str(), obj.get_password().c_str(), obj.get_edad(), obj.get_num_cel(), obj.get_modelo_auto().c_str(), obj.get_placa_auto().c_str());
+        fclose(archivo);
+    } else printf("~~ERROR, No se pudo abrir el archivo \"%s\"\n",nombre_arch.c_str());
+}
+void leerObjetosGuardados(std::string nombre_arch, std::vector<CUsuario>* lista) {
+    FILE* archivo = fopen(nombre_arch.c_str(), "r");
+    if(archivo != NULL) {
+        while(!feof(archivo)) {
+            char* nombre = new char[16];
+            char* apellido = new char[16];
+            char* contrasenia = new char[32];
+            short edad;
+            int num_cel;
+            fscanf(archivo, "%s %s %s %hi %d", nombre, apellido, contrasenia, &edad, &num_cel);
+            lista->push_back(CUsuario(nombre, apellido, contrasenia, edad, num_cel));
+            delete[] nombre;
+            delete[] apellido;
+            delete[] contrasenia;
         }
-        leer.close();
-        if(lista->get_t() > 1) lista->pop_back();
-
-    } else cout << "~~ERROR, No se pudo abrir el archivo \"" << archivo << "\"\n";
+        fclose(archivo);
+        if(lista->size() >= 2)
+            lista->pop_back(); // esto es para eliminar el objeto duplicado que siempre aparece al final
+    } else printf("~~ERROR, No se pudo abrir el archivo \"%s\"\n",nombre_arch.c_str());
+}
+void leerObjetosGuardados(std::string nombre_arch, std::vector<CTaxista>* lista) {
+    FILE* archivo = fopen(nombre_arch.c_str(), "r");
+    if(archivo != NULL) {
+        while(!feof(archivo)) {
+            char* nombre = new char[32];
+            char* apellido = new char[32];
+            char* contrasenia = new char[32];
+            short edad;
+            int num_cel;
+            char* modelo_auto = new char[32];
+            char* placa_auto = new char[32];
+            fscanf(archivo, "%s %s %s %hi %d %s %s", nombre, apellido, contrasenia, &edad, &num_cel, modelo_auto, placa_auto);
+            lista->push_back(CTaxista(nombre, apellido, contrasenia, edad, num_cel, CAutomovil(modelo_auto, placa_auto)));
+            delete[] nombre;
+            delete[] apellido;
+            delete[] contrasenia;
+            delete[] modelo_auto;
+            delete[] placa_auto;
+        }
+        fclose(archivo);
+        if(lista->size() >= 2)
+            lista->pop_back(); // esto es para eliminar el objeto duplicado que siempre aparece al final
+    } else printf("~~ERROR, No se pudo abrir el archivo \"%s\"\n",nombre_arch.c_str());
 }
