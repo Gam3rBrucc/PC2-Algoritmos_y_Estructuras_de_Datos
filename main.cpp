4156e3ceb32e7f32a08cf27554ec1416c8d7bcea
Bruce <brucematiasfleck@gmail.com>
2022-06-21 20:29:27 -0500

    Better main!

--- main.cpp
+++ main.cpp
@@ -1,21 +1,36 @@
 #include <iostream>
+#include <vector>
 #include "CMenu.h"
-
-using std::cout;
+#include "Archivo.h"
+#include "CUsuario.h"
+#include "CTaxista.h"
 
 int main() {
     srand(time(NULL));
 
-    cout << "===============================\n";
-    cout << "\tBienvendio a UPC Rider!\n";
-    cout << "===============================\n";
-    cout << "La app desarrollada por la mentes mas ingeniosas de la UPC.\n\n";
-    cout << "\tU20211E803 - Bruce Matias Fleck Ojeda\n";
-    cout << "\tU202116752 - Cesar Rafael Sanchez Garay\n";
-    cout << "\tU201921599 - Joaquin Enrique Mantilla Huaranga\n\n";
-    cout << "Esperamos le guste nuestro trabajo!\n\n";
+    std::vector<CUsuario> lista_usuarios;
+    std::vector<CTaxista> lista_taxistas;
 
-    CMenu();
+    leerObjetosGuardados("usuarios.txt", &lista_usuarios);
+    leerObjetosGuardados("taxistas.txt", &lista_taxistas);
+
+    printf("===============================\n");
+    printf("\tBienvendio a UPC Rider!\n");
+    printf("===============================\n");
+    printf("La app desarrollada por la mentes mas ingeniosas de la UPC.\n\n");
+    printf("\tU20211E803 - Bruce Matias Fleck Ojeda\n");
+    printf("\tU202116752 - Cesar Rafael Sanchez Garay\n");
+    printf("\tU201921599 - Joaquin Enrique Mantilla Huaranga\n\n");
+    printf("Esperamos le guste nuestro trabajo!\n\n");
+
+    CMenu(&lista_usuarios, &lista_taxistas);
+
+    FILE* f = fopen("usuarios.txt","w");
+    if(f != NULL) fclose(f);
+    FILE* g = fopen("taxistas.txt","w");
+    if(g != NULL) fclose(g);
+    for(auto i : lista_usuarios) guardarObjeto("usuarios.txt", i);
+    for(auto i : lista_taxistas) guardarObjeto("taxistas.txt", i);
 
     return 0;
 }
